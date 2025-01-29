98085856bfdd50341ae5a9b78b0373149aec3134
luff3 <vasylpeleshko13@gmail.com>
2025-01-29 16:04:17 +0200

    feat: add rxjs getting data and sorting it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,30 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CardConfigInterface, PrimaryCardComponent } from './components/primary-card/primary-card.component';
 import { ItemCardInterface } from './shared/interfaces/restaurant-card.interface';
-import { NgFor } from '@angular/common'; 
+import { NgFor, AsyncPipe } from '@angular/common'; 
 import { SecondaryCardComponent } from './components/secondary-card/secondary-card.component';
 import { RestaurantService } from './shared/services/restaurant.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fd-root',
-  imports: [RouterOutlet, MatSlideToggleModule, PrimaryCardComponent, SecondaryCardComponent, NgFor],
+  imports: [RouterOutlet, MatSlideToggleModule, PrimaryCardComponent, SecondaryCardComponent, NgFor, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'food-delivery';
-  restaurants: ItemCardInterface[] = [];
+  restaurants$!: Observable<ItemCardInterface[]>;
 
-  constructor(private restaurantsService: RestaurantService, private cdr: ChangeDetectorRef) {}
+  constructor(private restaurantsService: RestaurantService) {}
 
   ngOnInit(): void {
-    this.restaurantsService.getRestaurants().subscribe({
-      next: (data) => {
-        this.restaurants = data;
-        this.cdr.detectChanges();  
-      },
-      error: (err) => {
-        console.error('Помилка при отриманні ресторанів:', err);
-      }
-    });
+    this.restaurants$ = this.restaurantsService.getCachedRestaurants();
+    this.restaurantsService.getRestaurants().subscribe();
   }
+
   addToFavorites(restaurant: ItemCardInterface): void {
     this.restaurantsService.addToFavorites(restaurant);
     console.log(`${restaurant.name} added to favorites!`);
