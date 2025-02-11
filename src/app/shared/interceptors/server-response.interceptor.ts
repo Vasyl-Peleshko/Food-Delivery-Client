b8fa07b71d27541a66056626a569c5388c6e2c99
luff3 <vasylpeleshko13@gmail.com>
2025-02-11 16:02:57 +0200

    refactor: change logic of server-response interceptor

--- src/app/shared/interceptors/server-response.interceptor.ts
+++ src/app/shared/interceptors/server-response.interceptor.ts
@@ -4,33 +4,21 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpResponse,
   HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError } from 'rxjs/operators';
+import { LoadingService } from '../services/loading.service';
 
 @Injectable()
 export class ServerResponseInterceptor implements HttpInterceptor {
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private loadingService: LoadingService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-      tap(event => {
-        if ((request.method === "POST" || request.method === "GET") && event instanceof HttpResponse && event.status === 200) {            
-          this.snackBar.open('Success.', 'Close', {
-            duration: 2000,
-            panelClass: ['successSnack']
-          });
-        }
-      }),
       catchError((error: HttpErrorResponse) => {
         const errorMessage = error.error?.message || 'An unexpected error occurred.';
-        this.snackBar.open(errorMessage, 'Close', {
-          duration: 3000,
-          panelClass: ['errorSnack']
-        });
+        this.loadingService.showError(errorMessage);
         return throwError(() => error);
       })
     );
